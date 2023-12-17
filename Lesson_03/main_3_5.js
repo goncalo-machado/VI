a2919029be7b4d31e44a92fc24ceda0156fa591e
Gonçalo Machado <goncalofmachado01@gmail.com>
2023-12-17 19:53:43 +0000

    remove controls

--- Lesson_03/main_3_5.js
+++ Lesson_03/main_3_5.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,8 +6,6 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
-const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.SphereGeometry( 1, 32, 32 ); 
 
